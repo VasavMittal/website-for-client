ffee42f29c14c81453ea3fc6196291827c331077
Alvis Abreo <68607388+cratonoid@users.noreply.github.com>
2025-03-20 19:53:17 +0530

    fixed latest again

--- js/main.js
+++ js/main.js
@@ -56,13 +56,13 @@
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
+            $('.back-to-top').fadeIn('fast');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $('.back-to-top').fadeOut('fast');
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 10, 'easeInOutExpo');
         return false;
     });
 
